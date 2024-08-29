b530ceaa3e2c87be7251208872f9bf04fb5e195c
ivr88 <ivr88@yandex.ru>
2024-08-29 19:16:44 +0300

    Update ViewController.swift
    Настройка ViewController, проверка работоспособности приложения

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -1,17 +1,10 @@
-//
-//  ViewController.swift
-//  ToDoList
-//
-//  Created by Вадим Исламов on 29.08.2024.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
     }
 
 
