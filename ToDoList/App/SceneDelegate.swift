95e2dbf47f19343ca3fe88ef08756827804a8a19
ivr88 <ivr88@yandex.ru>
2024-08-31 19:03:40 +0300

    Update SceneDelegate.swift
    Обновил SceneDelegate для работы в VIPER

--- ToDoList/App/SceneDelegate.swift
+++ ToDoList/App/SceneDelegate.swift
@@ -7,8 +7,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowsScene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: windowsScene)
-        let taskViewController = TaskViewController()
-        window?.rootViewController = UINavigationController(rootViewController: taskViewController)
+        let taskModule = TaskRouter.createModule()
+        window?.rootViewController = UINavigationController(rootViewController: taskModule)
         window?.makeKeyAndVisible()
         
     }
