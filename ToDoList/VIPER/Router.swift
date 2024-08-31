c4dfa4eb45f1aa78cb407e2188d53af3b4d17d45
ivr88 <ivr88@yandex.ru>
2024-08-31 19:02:25 +0300

    Update Router.swift
    Добавил протокол и класс для Router

--- ToDoList/VIPER/Router.swift
+++ ToDoList/VIPER/Router.swift
@@ -1,8 +1,22 @@
-//
-//  Router.swift
-//  ToDoList
-//
-//  Created by Вадим Исламов on 31.08.2024.
-//
+import UIKit
 
-import Foundation
+protocol TaskRouterProtocol: AnyObject {
+    static func createModule() -> UIViewController
+}
+
+class TaskRouter: TaskRouterProtocol {
+    static func createModule() -> UIViewController {
+        let viewController = TaskViewController()
+        let presenter = TaskPresenter() 
+        let interactor = TaskInteractor()
+        let router: TaskRouterProtocol = TaskRouter()
+        
+        viewController.presenter = presenter
+        presenter.view = viewController
+        presenter.interactor = interactor
+        presenter.router = router
+        interactor.presenter = presenter
+        
+        return viewController
+    }
+}
