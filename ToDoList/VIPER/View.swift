b734f769fdad947c51038ac780a01716cb9e4425
ivr88 <ivr88@yandex.ru>
2024-08-31 18:44:06 +0300

    Update View.swift
    Обновил View

--- ToDoList/VIPER/View.swift
+++ ToDoList/VIPER/View.swift
@@ -1,17 +1,20 @@
 import UIKit
-import CoreData
+
+protocol TaskViewProtocol: AnyObject {
+    func displayTasks(_ tasks: [Task])
+}
 
 class TaskViewController: UIViewController {
     
+    var presenter: TaskPresenterProtocol?
+    
     private var tasks: [Task] = []
     private let tableView = UITableView()
-    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
 
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        loadTasksFromCoreData()
-        fetchTasks()
+        presenter?.viewDidLoad()
     }
 
     private func setupUI() {
@@ -24,111 +27,7 @@ class TaskViewController: UIViewController {
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Add Task", style: .plain, target: self, action: #selector(addTask))
     }
-    
-    private func loadTasksFromCoreData() {
-        let request: NSFetchRequest<TaskEntity> = TaskEntity.fetchRequest()
-        do {
-            let entities = try context.fetch(request)
-            tasks = entities.map { Task(from: $0) }
-            tableView.reloadData()
-        } catch {
-            print("Error loading tasks from Core Data: \(error)")
-        }
-    }
-    
-    private func saveTaskToCoreData(_ task: Task) {
-        let entity = TaskEntity(context: context)
-        entity.id = Int64(task.id)
-        entity.title = task.title
-        entity.isCompleted = task.isCompleted
-        saveContext()
-    }
-    
-    private func deleteTaskFromCoreData(at indexPath: IndexPath) {
-        let request: NSFetchRequest<TaskEntity> = TaskEntity.fetchRequest()
-        request.predicate = NSPredicate(format: "id == %d", tasks[indexPath.row].id)
-        
-        do {
-            if let entity = try context.fetch(request).first {
-                context.delete(entity)
-                saveContext()
-            }
-        } catch {
-            print("Error deleting task from Core Data: \(error)")
-        }
-    }
-    
-    private func updateTaskInCoreData(_ task: Task) {
-        let request: NSFetchRequest<TaskEntity> = TaskEntity.fetchRequest()
-        request.predicate = NSPredicate(format: "id == %d", task.id)
-        
-        do {
-            if let entity = try context.fetch(request).first {
-                entity.title = task.title
-                entity.isCompleted = task.isCompleted
-                saveContext()
-            }
-        } catch {
-            print("Error updating task in Core Data: \(error)")
-        }
-    }
-    
-    private func saveContext() {
-        do {
-            try context.save()
-        } catch {
-            print("Error saving Core Data context: \(error)")
-        }
-    }
-    
-    private func fetchTasks() {
-        let request: NSFetchRequest<TaskEntity> = TaskEntity.fetchRequest()
-        let count = try? context.count(for: request)
-        
-        if count ?? 0 > 0 {
-            return
-        }
-        DispatchQueue.global(qos: .background).async { [weak self] in
-            guard let url = URL(string: "https://dummyjson.com/todos") else {return}
-            let task = URLSession.shared.dataTask(with: url) { data, response, error in
-                if let error = error {
-                    print("Error fetching tasks: \(error)")
-                    return
-                }
-                
-                guard let data = data else { return }
-                
-                do {
-                    let decodedResponse = try JSONDecoder().decode(TodoResponse.self, from: data)
-                    DispatchQueue.main.async {
-                        self?.tasks = decodedResponse.todos
-                        self?.tasks.forEach { self?.saveTaskToCoreData($0) }
-                        self?.tableView.reloadData()
-                    }
-                } catch {
-                    print("Error decoding JSON: \(error)")
-                }
-            }
-            task.resume()
-        }
-    }
-    
-    private func addOrUpdateTask(_ newTask: Task) {
-        DispatchQueue.global(qos: .background).async { [weak self] in
-            if let existingTaskIndex = self?.tasks.firstIndex(where: { $0.id == newTask.id }) {
-                self?.tasks[existingTaskIndex] = newTask
-                self?.updateTaskInCoreData(newTask)
-            } else {
-                self?.tasks.append(newTask)
-                self?.saveTaskToCoreData(newTask)
-            }
 
-            DispatchQueue.main.async {
-                self?.tableView.reloadData()
-            }
-        }
-    }
-    
     @objc private func addTask() {
         let alert = UIAlertController(title: "New Task", message: "Enter task details", preferredStyle: .alert)
         alert.addTextField { textField in
@@ -137,12 +36,7 @@ class TaskViewController: UIViewController {
         
         let addAction = UIAlertAction(title: "Add", style: .default) { [weak self] _ in
             guard let title = alert.textFields?.first?.text, !title.isEmpty else { return }
-            
-            DispatchQueue.global(qos: .background).async {
-                let newTaskID = (self?.tasks.map { $0.id }.max() ?? 0) + 1
-                let newTask = Task(id: newTaskID, title: title, isCompleted: false)
-                self?.addOrUpdateTask(newTask)
-            }
+            self?.presenter?.addTask(withTitle: title)
         }
         
         alert.addAction(addAction)
@@ -150,7 +44,7 @@ class TaskViewController: UIViewController {
         present(alert, animated: true)
     }
     
-    private func editTask(at indexPath: IndexPath) {
+    func editTask(at indexPath: IndexPath) {
         let task = tasks[indexPath.row]
         
         let alert = UIAlertController(title: "Edit Task", message: nil, preferredStyle: .alert)
@@ -160,15 +54,7 @@ class TaskViewController: UIViewController {
         
         let saveAction = UIAlertAction(title: "Save", style: .default) { [weak self] _ in
             guard let newTitle = alert.textFields?.first?.text, !newTitle.isEmpty else { return }
-            
-            DispatchQueue.global(qos: .background).async {
-                self?.tasks[indexPath.row].title = newTitle
-                self?.updateTaskInCoreData(self!.tasks[indexPath.row])
-                
-                DispatchQueue.main.async {
-                    self?.tableView.reloadData()
-                }
-            }
+            self?.presenter?.editTask(at: indexPath.row, withTitle: newTitle)
         }
         
         alert.addAction(saveAction)
@@ -176,42 +62,19 @@ class TaskViewController: UIViewController {
         present(alert, animated: true)
     }
     
-    private func deleteTask(at indexPath: IndexPath) {
-        DispatchQueue.global(qos: .background).async { [weak self] in
-            self?.deleteTaskFromCoreData(at: indexPath)
-            self?.tasks.remove(at: indexPath.row)
-            
-            DispatchQueue.main.async {
-                self?.tableView.reloadData()
-            }
-        }
+    func deleteTask(at indexPath: IndexPath) {
+        presenter?.deleteTask(at: indexPath.row)
     }
     
-    private func toggleTaskCompletion(at indexPath: IndexPath) {
-        DispatchQueue.global(qos: .background).async { [weak self] in
-            self?.tasks[indexPath.row].isCompleted.toggle()
-            self?.updateTaskInCoreData(self!.tasks[indexPath.row])
-            
-            DispatchQueue.main.async {
-                self?.tableView.reloadRows(at: [indexPath], with: .automatic)
-            }
-        }
+    func toggleTaskCompletion(at indexPath: IndexPath) {
+        presenter?.toggleTaskCompletion(at: indexPath.row)
     }
-    
-    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
-        let editAction = UIContextualAction(style: .normal, title: "Edit") { [weak self] _, _, completionHandler in
-            self?.editTask(at: indexPath)
-            completionHandler(true)
-        }
-        editAction.backgroundColor = .systemBlue
-        
-        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { [weak self] _, _, completionHandler in
-            self?.deleteTask(at: indexPath)
-            completionHandler(true)
-        }
-        
-        let configuration = UISwipeActionsConfiguration(actions: [deleteAction, editAction])
-        return configuration
+}
+
+extension TaskViewController: TaskViewProtocol {
+    func displayTasks(_ tasks: [Task]) {
+        self.tasks = tasks
+        tableView.reloadData()
     }
 }
 
@@ -231,5 +94,21 @@ extension TaskViewController: UITableViewDataSource, UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         toggleTaskCompletion(at: indexPath)
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let editAction = UIContextualAction(style: .normal, title: "Edit") { [weak self] _, _, completionHandler in
+            self?.editTask(at: indexPath)
+            completionHandler(true)
+        }
+        editAction.backgroundColor = .systemBlue
+        
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { [weak self] _, _, completionHandler in
+            self?.deleteTask(at: indexPath)
+            completionHandler(true)
+        }
+        
+        let configuration = UISwipeActionsConfiguration(actions: [deleteAction, editAction])
+        return configuration
     }
 }
