7c995f51f455f98cd226656cdd57cfd9d9ee0694
ivr88 <ivr88@yandex.ru>
2024-08-31 18:51:50 +0300

    Update Presenter.swift
    Ввёл необходимые методы в Presenter

--- ToDoList/VIPER/Presenter.swift
+++ ToDoList/VIPER/Presenter.swift
@@ -1,8 +1,46 @@
-//
-//  Presenter.swift
-//  ToDoList
-//
-//  Created by Вадим Исламов on 31.08.2024.
-//
-
 import Foundation
+
+protocol TaskPresenterProtocol: AnyObject {
+    func viewDidLoad()
+    func addTask(withTitle title: String)
+    func editTask(at index: Int, withTitle title: String)
+    func deleteTask(at index: Int)
+    func toggleTaskCompletion(at index: Int)
+}
+
+protocol TaskInteractorOutputProtocol: AnyObject {
+    func didFetchTasks(_ tasks: [Task])
+}
+
+class TaskPresenter: TaskPresenterProtocol {
+
+    weak var view: TaskViewProtocol?
+    var interactor: TaskInteractorProtocol?
+    var router: TaskRouterProtocol?
+    
+    func viewDidLoad() {
+        interactor?.fetchTasks()
+    }
+    
+    func addTask(withTitle title: String) {
+        interactor?.addTask(withTitle: title)
+    }
+    
+    func editTask(at index: Int, withTitle title: String) {
+        interactor?.editTask(at: index, withTitle: title)
+    }
+    
+    func deleteTask(at index: Int) {
+        interactor?.deleteTask(at: index)
+    }
+    
+    func toggleTaskCompletion(at index: Int) {
+        interactor?.toggleTaskCompletion(at: index)
+    }
+}
+
+extension TaskPresenter: TaskInteractorOutputProtocol {
+    func didFetchTasks(_ tasks: [Task]) {
+        view?.displayTasks(tasks)
+    }
+}
