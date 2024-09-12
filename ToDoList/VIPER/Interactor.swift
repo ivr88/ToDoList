903344708945066bc092f45793e0a03ca7f0a26b
ivr88 <ivr88@yandex.ru>
2024-09-12 15:36:54 +0300

    Update Interactor.swift
    Внёс изменения в Interactor после выделения coreDataService и apiService в отдельные сущности

--- ToDoList/VIPER/Interactor.swift
+++ ToDoList/VIPER/Interactor.swift
@@ -13,16 +13,22 @@ protocol TaskInteractorProtocol: AnyObject {
 class TaskInteractor: TaskInteractorProtocol {
     
     weak var presenter: TaskInteractorOutputProtocol?
-    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    private let coreDataService: TaskServiceProtocol
+    private let apiService: TaskAPIServiceProtocol
     private var tasks: [Task] = []
     
+    init(coreDataService: TaskServiceProtocol, apiService: TaskAPIServiceProtocol) {
+        self.coreDataService = coreDataService
+        self.apiService = apiService
+    }
+
     func fetchTasks() {
-        loadTasksFromCoreData()
+        tasks = coreDataService.fetchTasks()
         if tasks.isEmpty {
-            fetchTasksFromAPI()
-        } else {
-            presenter?.didFetchTasks(tasks)
+            tasks = apiService.fetchTasks()
+            tasks.forEach { coreDataService.saveTask($0) }
         }
+        presenter?.didFetchTasks(tasks)
     }
     
     func addTask(withTitle title: String) {
@@ -33,110 +39,29 @@ class TaskInteractor: TaskInteractorProtocol {
     
     func editTask(at index: Int, withTitle title: String) {
         tasks[index].title = title
-        updateTaskInCoreData(tasks[index])
+        coreDataService.updateTask(tasks[index])
         presenter?.didFetchTasks(tasks)
     }
     
     func deleteTask(at index: Int) {
-        deleteTaskFromCoreData(at: index)
+        coreDataService.deleteTask(withID: tasks[index].id)
         tasks.remove(at: index)
         presenter?.didFetchTasks(tasks)
     }
     
     func toggleTaskCompletion(at index: Int) {
         tasks[index].isCompleted.toggle()
-        updateTaskInCoreData(tasks[index])
+        coreDataService.updateTask(tasks[index])
         presenter?.didFetchTasks(tasks)
-    }
-    
-    private func loadTasksFromCoreData() {
-        let request: NSFetchRequest<TaskEntity> = TaskEntity.fetchRequest()
-        do {
-            let entities = try context.fetch(request)
-            tasks = entities.map { Task(from: $0) }
-        } catch {
-            print("Error loading tasks from Core Data: \(error)")
-        }
-    }
-    
-    private func saveTaskToCoreData(_ task: Task) {
-        let entity = TaskEntity(context: context)
-        entity.id = Int64(task.id)
-        entity.title = task.title
-        entity.isCompleted = task.isCompleted
-        saveContext()
-    }
-    
-    private func deleteTaskFromCoreData(at index: Int) {
-        let request: NSFetchRequest<TaskEntity> = TaskEntity.fetchRequest()
-        request.predicate = NSPredicate(format: "id == %d", tasks[index].id)
-        
-        do {
-            if let entity = try context.fetch(request).first {
-                context.delete(entity)
-                saveContext()
-            }
-        } catch {
-            print("Error deleting task from Core Data: \(error)")
-        }
-    }
-    
-    private func updateTaskInCoreData(_ task: Task) {
-        let request: NSFetchRequest<TaskEntity> = TaskEntity.fetchRequest()
-        request.predicate = NSPredicate(format: "id == %d", task.id)
-        
-        do {
-            if let entity = try context.fetch(request).first {
-                entity.title = task.title
-                entity.isCompleted = task.isCompleted
-                saveContext()
-            }
-        } catch {
-            print("Error updating task in Core Data: \(error)")
-        }
-    }
-    
-    private func saveContext() {
-        do {
-            try context.save()
-        } catch {
-            print("Error saving Core Data context: \(error)")
-        }
-    }
-    
-    private func fetchTasksFromAPI() {
-        DispatchQueue.global(qos: .background).async { [weak self] in
-            guard let url = URL(string: "https://dummyjson.com/todos") else {return}
-            let task = URLSession.shared.dataTask(with: url) { data, response, error in
-                if let error = error {
-                    print("Error fetching tasks: \(error)")
-                    return
-                }
-                
-                guard let data = data else { return }
-                
-                do {
-                    let decodedResponse = try JSONDecoder().decode(TodoResponse.self, from: data)
-                    DispatchQueue.main.async {
-                        self?.tasks = decodedResponse.todos
-                        self?.tasks.forEach { self?.saveTaskToCoreData($0) }
-                        self?.presenter?.didFetchTasks(self?.tasks ?? [])
-                    }
-                } catch {
-                    print("Error decoding JSON: \(error)")
-                }
-            }
-            task.resume()
-        }
     }
     
     private func addOrUpdateTask(_ newTask: Task) {
         if let existingTaskIndex = tasks.firstIndex(where: { $0.id == newTask.id }) {
             tasks[existingTaskIndex] = newTask
-            updateTaskInCoreData(newTask)
+            coreDataService.updateTask(newTask)
         } else {
             tasks.append(newTask)
-            saveTaskToCoreData(newTask)
+            coreDataService.saveTask(newTask)
         }
         presenter?.didFetchTasks(tasks)
     }
