06b88cbcf4741970d9595d7a42a2edc83efa2076
ivr88 <ivr88@yandex.ru>
2024-09-12 15:38:00 +0300

    Update Router.swift
    Обновил Router после внесения изменений в Interactor

--- ToDoList/VIPER/Router.swift
+++ ToDoList/VIPER/Router.swift
@@ -7,16 +7,16 @@ protocol TaskRouterProtocol: AnyObject {
 class TaskRouter: TaskRouterProtocol {
     static func createModule() -> UIViewController {
         let viewController = TaskViewController()
-        let presenter = TaskPresenter() 
-        let interactor = TaskInteractor()
+        let presenter = TaskPresenter()
+        let coreDataService = CoreDataService()
+        let apiService = APIService()
+        let interactor = TaskInteractor(coreDataService: coreDataService, apiService: apiService)
         let router: TaskRouterProtocol = TaskRouter()
-        
         viewController.presenter = presenter
         presenter.view = viewController
         presenter.interactor = interactor
         presenter.router = router
         interactor.presenter = presenter
-        
         return viewController
     }
 }
