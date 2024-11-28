a971fc7bcdb89a050a970ef440e12f9203643ae0
ivr88 <ivr88@yandex.ru>
2024-11-28 21:43:11 +0300

    Create TaskEditRouter.swift
    Создал TaskEditRouter

---
+++ ToDoList/TaskEditModule/TaskEditRouter.swift
@@ -0,0 +1,23 @@
+import UIKit
+
+protocol TaskEditRouterProtocol: AnyObject {
+    static func createModule(withTask task: Task) -> UIViewController
+}
+
+class TaskEditRouter: TaskEditRouterProtocol {
+    static func createModule(withTask task: Task) -> UIViewController {
+        let viewController = TaskEditViewController()
+        let presenter = TaskEditPresenter()
+        let interactor = TaskEditInteractor(repository: TaskRepository(coreDataService: CoreDataService(), apiService: APIService()))
+        let router = TaskEditRouter()
+        
+        presenter.view = viewController
+        presenter.interactor = interactor
+        presenter.router = router
+        presenter.setTask(task)
+        
+        viewController.presenter = presenter
+        
+        return viewController
+    }
+}
