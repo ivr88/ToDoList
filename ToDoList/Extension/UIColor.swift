ec89ac2a563460f21ea52909500532d59a9d61e0
ivr88 <ivr88@yandex.ru>
2024-12-01 20:24:59 +0300

    Create UIColor.swift
    Создал расширение для  UIColor

---
+++ ToDoList/Extension/UIColor.swift
@@ -0,0 +1,7 @@
+import UIKit
+
+//#colorLiteral()
+extension UIColor {
+    static let customGray = #colorLiteral(red: 0.1529411765, green: 0.1529411765, blue: 0.1607843137, alpha: 1)
+    static let customTellow = #colorLiteral(red: 0.9960784314, green: 0.8431372549, blue: 0.007843137255, alpha: 1)
+}
