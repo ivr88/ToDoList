e8384179ef069d55cb3e92bc380ffa99100a665b
ivr88 <ivr88@yandex.ru>
2024-12-04 00:48:16 +0300

    Update TaskEditRouter.swift
    Переработал Роутер под навигацию

--- ToDoList/TaskEditModule/TaskEditRouter.swift
+++ ToDoList/TaskEditModule/TaskEditRouter.swift
@@ -1,23 +1,11 @@
 import UIKit
 
 protocol TaskEditRouterProtocol: AnyObject {
-    static func createModule(withTask task: Task) -> UIViewController
+    func navigateBackToTaskList(from viewController: UIViewController)
 }
 
 final class TaskEditRouter: TaskEditRouterProtocol {
-    static func createModule(withTask task: Task) -> UIViewController {
-        let viewController = TaskEditViewController()
-        let presenter = TaskEditPresenter()
-        let interactor = TaskEditInteractor(repository: TaskRepository(coreDataService: CoreDataService(), apiService: APIService()))
-        let router = TaskEditRouter()
-        
-        presenter.view = viewController
-        presenter.interactor = interactor
-        presenter.router = router
-        presenter.setTask(task)
-        
-        viewController.presenter = presenter
-        
-        return viewController
+    func navigateBackToTaskList(from viewController: UIViewController) {
+        viewController.navigationController?.popViewController(animated: true)
     }
 }
